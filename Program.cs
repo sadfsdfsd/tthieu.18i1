c68a6f7aa59fcecb637ce944c5198c6aaaeed834
tthieu18i1 <48077337+sadfsdfsd@users.noreply.github.com>
2019-02-27 19:42:04 -0800

    Add files via upload
    tthieu18i1
    nộp bài tập

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Check
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            tinhtong x = new tinhtong();
+            x.tong();
+            x.In();
+            Console.ReadLine()
+        }
+    }
+}
